2154b99acafb6030b8534240b613bcfea0ef8605
SamPerCap <smperez190@gmail.com>
2024-06-28 11:05:04 +0200

    Add project files.

---
+++ Data/LeaveManagementDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagementSystem.Data
+{
+    public class LeaveManagementDbContext : IdentityDbContext
+    {
+        public LeaveManagementDbContext(DbContextOptions<LeaveManagementDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+    }
+}
---
+++ Data/LeaveType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementSystem.Data
+{
+    public class LeaveType
+    {
+        [Key]
+        public int LeaveTypeId { get; set; }
+        [Display(Name = "Leave type name")]
+        [Column(TypeName = "nvarchar(150)")]
+        public string LeaveTypeName { get; set; }
+        [Display(Name = "Number of days")]
+        public int NumberOfDays { get; set; }
+    }
+}
